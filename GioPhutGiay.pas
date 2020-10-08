fc8b73041d465b26e704ccb90bc0a1ee99ec0b0d
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-10-08 21:34:19 +0700

    Bài tập 1 - Ngày 08/10/2020

---
+++ GioPhutGiay.pas
@@ -0,0 +1,18 @@
+program test;
+const f1='clock.in';
+      f2='clock.out';
+var
+    n: longint;
+    h,p,s: integer;
+begin
+    assign(input, f1); reset(input);
+    assign(output, f2); rewrite(output);
+    {Assign default values for variables}
+    h:=0; p:=0; s:=0;
+    readln(n);
+    h:=n div 3600;
+    p:=(n mod 3600) div 60;
+    s:=n mod 60;
+    writeln(h,' gio ', p, ' phut ', s, ' giay');
+    close(input); close(output);
+end.
