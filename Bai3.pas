e0b1d8094a716869780689c67645799b50c402d1
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-10-09 22:35:42 +0700

    Bài tập 3 - Hoàn thành 09/10/2020
    Nhập vào số n trong file dtn.in [input]
    In ra file dtn.out tất cả các số nguyên tố nhỏ hơn hoặc bằng n

---
+++ Bai3.pas
@@ -0,0 +1,30 @@
+program test;
+const f1='dnt.in';
+      f2='dnt.out';
+var
+    n: integer;
+    x, y: integer;
+    b: boolean;
+begin
+    assign(input, f1); reset(input);
+    assign(output, f2); rewrite(output);
+    readln(n);
+    if n < 2 then
+        writeln('Khong co so nguyen to nao nho hon 2!')
+    else
+        begin
+            for x:=2 to n do
+            begin
+                b:=true;
+                for y:=2 to x - 1 do
+                begin
+                    if x mod y = 0 then
+                    b:=false;
+                    break
+                end;
+                if b then
+                write(x, ' ');
+            end;
+        end;
+    close(input); close(output);
+end.
