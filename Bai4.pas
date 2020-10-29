6274a3a356fae158c2370a6961e1c0f5a9bbc620
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-10-29 12:33:16 +0700

    Cập nhật

--- Bai4.pas
+++ Bai4.pas
@@ -1,19 +1,20 @@
 program bt4;
-const f1='pt.in';
-      f2='pt.out';
+const f1='pt.in'; f2='pt.out';
 var
-   a,b: integer;
+  a,b: real;
+  x: real;
 begin
-   assign(input, f1); reset(input);
-   assign(output, f2); rewrite(output);
-   readln(a,b);
-   if (b = 0) and (a = 0) then
-     writeln('Phuong trinh co vo so nghiem!')
-    else if (b = 0) and (a <> 0) then
-      writeln('x co gia tri bang 0')
-    else if (b <> 0) and (a = 0) then
-      writeln('Phuong trinh khong co nghiem!')
+  assign(input, f1); reset(input); readln(a,b);
+  assign(output, f2); rewrite(output);
+  if(b = 0) then
+    writeln('Phuong trinh co nghiem la 0!')
+  else
+  begin
+    if(a = 0) then
+      writeln('Phuong trinh vo nghiem!')
     else
-      writeln('x co gia tri ', (b * -1) div a);
-    close(input); close(output);
+      x:=(b*(-1)) / a;
+      writeln('Phuong trinh co nghiem x = ', x:0:2);
+  end;
+  close(input); close(output);
 end.
