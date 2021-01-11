94e8b2063f112f24dbee0216c3daea61c2923678
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2021-01-11 18:30:45 +0700

    optimize code

--- Cau4_3.pas
+++ Cau4_3.pas
@@ -7,8 +7,8 @@ var
 begin
     assign(input, f1); reset(input); readln(s1); readln(s2);
     assign(output, f2); rewrite(output);
-    s1:= LowerCase(s1); s2:=LowerCase(s2);
     len:=Length(s1); bool:=true;
+    s1:=UpCase(s1); s2:=UpCase(s2);
     for i:=1 to len do begin
         if Pos(s1[i], s2) = 0 then bool:=false; 
     end;
@@ -20,7 +20,7 @@ begin
     if bool then begin
         for i:=1 to Length(s1) do
             if Pos(s1[i], s3) = 0 then s3:=s3+s1[i];
-    end;
-    if bool then begin writeln('YES'); write(UpCase(s3)); end else write('NO');
+        writeln('YES'); write(s3);
+    end; else write('NO');
     close(input); close(output);
 end.
