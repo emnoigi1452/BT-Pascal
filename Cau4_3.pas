02d473bf893a6c2b59f87b561827e905a8002d9b
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2021-01-11 18:38:14 +0700

    Dọn code / Sửa bố cục

--- Cau4_3.pas
+++ Cau4_3.pas
@@ -1,26 +1,21 @@
-program test;
+program bt;
 const f1='bt.in'; f2='bt.out';
 var
     s1,s2,s3: string;
-    i,len: integer;
     bool: boolean;
+    i: integer;
 begin
-    assign(input, f1); reset(input); readln(s1); readln(s2);
+    assign(input, f1); reset(input);
     assign(output, f2); rewrite(output);
-    len:=Length(s1); bool:=true;
-    s1:=UpCase(s1); s2:=UpCase(s2);
-    for i:=1 to len do begin
-        if Pos(s1[i], s2) = 0 then bool:=false; 
-    end;
-    len:=Length(s2);
-    for i:=1 to len do begin
-        if Pos(s2[i], s1) = 0 then bool:=false;
-    end;
-    s3 := '';
-    if bool then begin
-        for i:=1 to Length(s1) do
-            if Pos(s1[i], s3) = 0 then s3:=s3+s1[i];
+    readln(s1); s1:=UpCase(s1);
+    readln(s2); s2:=UpCase(s2);
+    bool:= true;
+    for i:=1 to Length(s1) do if Pos(s1[i], s2) = 0 then bool:= false;
+    for i:=1 to Length(s2) do if Pos(s2[i], s1) = 0 then bool:= false;
+    if not bool then write('NO') else begin
+        s3 := '';
+        for i:=1 to Length(s1) do if Pos(s1[i], s3) = 0 then s3:=s3+s1[i];
         writeln('YES'); write(s3);
-    end; else write('NO');
+    end;
     close(input); close(output);
 end.
