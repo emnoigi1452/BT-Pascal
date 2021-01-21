5e5a8e7b34df0009e46659b9d55978725865bdaa
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2021-01-21 20:16:30 +0700

    Bài 17 : Tạo mảng theo công thức

--- TAOMANG.pas
+++ TAOMANG.pas
@@ -9,7 +9,6 @@ begin
   assign(output, f2); rewrite(output); sum:=0;
   for k1:=1 to n do begin
     i:=1; for k2:=1 to k1 do i:=i*a; i:=(i mod m) + 1;
-    sum:=sum+i;
-  end;
+  end; sum:=sum + i;
   write(sum); close(input); close(output);
 end.
